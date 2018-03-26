d3af3633e14fc83b3328c253610031567e93aece
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-03-26 07:16:04 +0200

    add star wars theme

--- src/app.js
+++ src/app.js
@@ -10,11 +10,20 @@ import pageJS from './slides/000-page-js.slide';
 import pageHTML from './slides/000-page-html.slide.html';
 import pageMarkDown from './slides/000-page-md.slide.md';
 
+// Star Wars Theme
+import swLead from './slides/100-sw-lead.slide.html';
+import swTitle from './slides/100-sw-title.slide.html';
+import swOpening from './slides/100-sw-opening.slide.html';
+
 // -------------------------------------------------------------------------
 // Slides
 // -------------------------------------------------------------------------
 
 const slides = [
+  swLead,
+  swTitle,
+  swOpening,
+
   pageJS,
   pageHTML,
   pageMarkDown,
